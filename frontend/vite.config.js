fe9cf36b0a5d7d9b3ec28f632f89342da671a7b1
codemonic-brains-zero <piyushmfalke@gmail.com>
2024-11-08 20:00:13 +0530

    check the changes

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,5 +3,9 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  root: 'frontend/src', // Set the root to 'src' inside the frontend folder
   plugins: [react()],
+  build: {
+    outDir: 'frontend/dist',  // This ensures the build files go into the dist folder
+  },
 })
